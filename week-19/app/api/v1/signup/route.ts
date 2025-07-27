36ad7d62de9414ece86262a2b983431481be4c5c
areyyynitin <np089250@gmail.com>
2025-07-27 09:20:36 +0000

    concept of fronted,backend,prisma

--- week-19/app/api/v1/signup/route.ts
+++ week-19/app/api/v1/signup/route.ts
@@ -1,9 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
+import prismaClient from "@/app/lib/db"
 
-export async function POST(req:NextRequest){
-    const  data  = await req.json()
-    console.log(data)
-  return  NextResponse.json({
-      message:"You have been signed up"
+
+
+export async function POST(req: NextRequest) {
+  try {
+    
+    const data = await req.json();
+    await prismaClient.user.create({
+      data: {
+        username: data.username,
+        password: data.password,
+      },
+    });
+    console.log(data);
+    return NextResponse.json({
+      message: "You have been signed up",
+    });
+  }  catch (error) {
+    return NextResponse.json({
+      message:`Error while signed up ${error}`
     })
+  }
 }
--- week-19/app/signup/page.tsx
+++ week-19/app/signup/page.tsx
@@ -1,24 +1,32 @@
 "use client"
 
 import axios from "axios"
+import { useRouter } from "next/navigation"
 import { useState } from "react"
 
 export default function Signup() {
-    const [username,setUsername] = useState("")
-    const [password,setPassword] = useState("")
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const router = useRouter()
+
+    const getUserDetails = async () => {
+        console.log("doinfndskewrfdoi..............................", username, password)
+        await axios.post("http://localhost:3000/api/v1/signup", {
+            username, password
+        })
+        router.push("/signin")
+    }
+
+
 
     return <>
         <div className="w-screen h-screen flex justify-center items-center ">
             <div className="border p-2 ">
                 <input onChange={(e) => setUsername(e.target.value)} value={username} type="text" placeholder="username" />
                 <input onChange={(e) => setPassword(e.target.value)} value={password} type="password" placeholder="password" />
-                <button onClick={() => { 
-                    console.log("doinfndskewrfdoi..............................",username,password)
-                    axios.post("http://localhost:3000/api/v1/signup" , {
-                        username,password
-                    })
-                }}>Sign up</button>
+                <button onClick={getUserDetails}>Sign up</button>
             </div>
-        </div>
+        </div >
     </>
 }
+
